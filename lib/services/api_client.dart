3128b1eef4ed8812d49cc71dff45763077ba585c
start <mrcs.stark@gmail.com>
2019-03-06 04:02:13 -0300

    Adicionando plugins e criando modelo

---
+++ lib/services/api_client.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'dart:async';
+
+
+const API_KEY = "AIzaSyDJjvfseoNR5ot7fSDBXA7ub2iiCWFuz60";
+
+/*
+* URLs Usadas
+*
+* 1 - "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$API_KEY&maxResults=10"
+* 2 - "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$_search&type=video&key=$API_KEY&maxResults=10&pageToken=$_nextToken"
+* 3 "http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1&q=$search&format=5&alt=json"
+* */
+
+class ApiClient {
+
+  search(String term) async {
+    http.Response response = await http.get(
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$term&type=video&key=$API_KEY&maxResults=10"
+    );
+
+    _decodeToJson(response);
+
+  }
+
+  void _decodeToJson(http.Response response) {
+    if(response.statusCode == 200){
+      var decoded = json.decode(response.body);
+
+
+
+    }
+  }
+}
